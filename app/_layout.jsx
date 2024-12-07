b37d37f066dcf0dc61026162c3b7e636d0096581
MN Raza <mnraza94@gmail.com>
2024-12-07 17:40:29 +0530

    first commit

---
+++ app/_layout.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }} />
+      <Stack.Screen name='profile' options={{ headerShown: false }} />
+    </Stack>
+  )
+}
+
+export default RootLayout
+
+const styles = StyleSheet.create({})
