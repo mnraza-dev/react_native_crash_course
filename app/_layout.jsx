fcd644e3d27df496d03d07ed0900e32ad416342e
MN Raza <mnraza94@gmail.com>
2024-12-07 17:45:07 +0530

    learnt Linking to the screens

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -6,7 +6,7 @@ const RootLayout = () => {
   return (
     <Stack>
       <Stack.Screen name='index' options={{ headerShown: false }} />
-      <Stack.Screen name='profile' options={{ headerShown: false }} />
+      <Stack.Screen name='profile' />
     </Stack>
   )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const App = () => {
   return (
-    <View>
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
       <Text>App</Text>
+      <Link href='/profile'>Go to Profile</Link>
     </View>
   )
 }
